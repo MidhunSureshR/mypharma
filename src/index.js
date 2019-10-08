8afea7ee52952269f1b2dec37a24d7e2cbed382a
MidhunSureshR <thedeceptionist@hotmail.com>
2019-10-08 20:29:05 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactFullpage from '@fullpage/react-fullpage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Fullpage = () => (
+  <ReactFullpage
+    //fullpage options
+    licenseKey = {'YOUR_KEY_HERE'}
+    scrollingSpeed = {1000} /* Options here */
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    render={({ state, fullpageApi }) => {
+      return (
+        <ReactFullpage.Wrapper>
+          <div className="section land-pic">
+            
+                        <h1 className="display-1 font-weight-bolder catch-text slide-in-top">Pharmacy. Amplified</h1>
+          </div>
+
+           <div className="section">
+              <div className="a">
+                <h1 className="display-1 font-weight-bolder catch-text padded">Reliable</h1>
+                <h1 className="display-1 font-weight-bolder catch-text padded">Fast Delivery</h1>
+                <h1 className="display-1 font-weight-bolder catch-text padded">Discounts</h1>
+              </div>
+          </div> 
+
+        </ReactFullpage.Wrapper>
+      );
+    }}
+  />
+);
+
+ReactDOM.render(<Fullpage />, document.getElementById('react-root'));
+// class Logo extends React.Component {
+   
+//     render() {
+      
+//         return (
+//          <nav class="nav-bar">
+//            <a href="#" class="nav-time"> Home  </a> | 
+//            <a href="#" class="nav-time"> Login  </a> | 
+//            <a href="#" class="nav-time"> Sign-up  </a> |  
+//            <a href="#" class="nav-time"> About  </a> | 
+//            <a href="#" class="nav-time"> Github  </a> | 
+//          </nav>
+//         );
+//       }
+    
+    
+//   }
+
+  //ReactDOM.render(<Logo/>, document.getElementById("navbar"));
