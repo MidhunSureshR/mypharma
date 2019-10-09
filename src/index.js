ab002b748fabcf346957e666217ada4cce454c23
MidhunSureshR <thedeceptionist@hotmail.com>
2019-10-09 18:23:08 +0530

    Added Signup form.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,103 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactFullpage from '@fullpage/react-fullpage';
 
+
+class SignupForm extends React.Component{
+  render(){
+    return(
+
+     <React.Fragment>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"/>
+
+
+<div class="container">
+<br/>  <p class="text-center">Signup with MYPHARMA today.</p>
+<hr/>
+
+
+
+
+
+<div class="card" id="box-reg">
+<article class="card-body mx-auto" style={{maxWidth: '400px'}}>
+	<h4 class="card-title mt-3 text-center">Create Account</h4>
+	<p class="text-center">Get started with your free account</p>
+	
+	<form>
+	<div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+        <input name="" class="form-control" placeholder="Full name" type="text"/>
+    </div> 
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+		 </div>
+        <input name="" class="form-control" placeholder="Email address" type="email"/>
+    </div> 
+
+    <div class="form-group input-group">
+
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-phone"></i></span>
+        <span class="input-group-text">+91</span>
+		  </div>
+
+    	<input name="" class="form-control" placeholder="Phone number" type="text"/>
+
+    </div> 
+    
+    <div class="form-group input-group">
+
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-building"></i> </span>
+		  </div>
+
+      <input type="text" class="form-control" id="inputAddress2" placeholder="Address Line 1"/>
+      
+
+	</div> 
+
+  <div class="form-group input-group">
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Address Line 2"/>
+  </div>
+
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input class="form-control" placeholder="Create password" type="password"/>
+    </div> 
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input class="form-control" placeholder="Repeat password" type="password"/>
+    </div>                                    
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-block"> Create Account  </button>
+    </div>    
+    <p class="text-center">Have an account? <a href="#">Log In</a> </p>                                                                 
+</form>
+</article>
+</div> 
+
+</div> 
+
+
+<br/><br/>
+
+</React.Fragment>
+    );
+      
+    }
+  }
+
+
 const Fullpage = () => (
   <ReactFullpage
     //fullpage options
@@ -17,10 +114,9 @@ const Fullpage = () => (
           </div>
 
            <div className="section">
-              <div className="a">
-                <h1 className="display-1 font-weight-bolder catch-text padded">Reliable</h1>
-                <h1 className="display-1 font-weight-bolder catch-text padded">Fast Delivery</h1>
-                <h1 className="display-1 font-weight-bolder catch-text padded">Discounts</h1>
+              <div className="reg-page">
+                    {/** Registration component will appear here */}
+                    <SignupForm/>
               </div>
           </div> 
 
@@ -30,23 +126,7 @@ const Fullpage = () => (
   />
 );
 
-ReactDOM.render(<Fullpage />, document.getElementById('react-root'));
-// class Logo extends React.Component {
-   
-//     render() {
-      
-//         return (
-//          <nav class="nav-bar">
-//            <a href="#" class="nav-time"> Home  </a> | 
-//            <a href="#" class="nav-time"> Login  </a> | 
-//            <a href="#" class="nav-time"> Sign-up  </a> |  
-//            <a href="#" class="nav-time"> About  </a> | 
-//            <a href="#" class="nav-time"> Github  </a> | 
-//          </nav>
-//         );
-//       }
-    
-    
-//   }
 
-  //ReactDOM.render(<Logo/>, document.getElementById("navbar"));
+
+ReactDOM.render(<Fullpage />, document.getElementById('react-root'));
+
